e35973270778a2372a011b44f58a1a14bc451d9c
christopherkim2273 <christopher2273@gmail.com>
2022-04-13 19:20:31 -0400

    Removed modifications to unmodifiable Point class

--- src/geometry/Point.java
+++ src/geometry/Point.java
@@ -1,8 +1,5 @@
 package geometry;
 
-import java.math.BigDecimal;
-import java.math.RoundingMode;
-
 /**
  * An unmodifiable point in the standard two-dimensional Euclidean space. The coordinates of such a point is given by
  * exactly two doubles specifying its <code>x</code> and <code>y</code> values.
@@ -24,12 +21,5 @@ public class Point {
     
     public double getY() {
         return this.y;
-    }
-
-    @Override
-    public String toString(){
-        String printable_x = BigDecimal.valueOf(x).setScale(3, RoundingMode.HALF_UP).toPlainString();
-        String printable_y = BigDecimal.valueOf(y).setScale(3, RoundingMode.HALF_UP).toPlainString();
-        return ("("+printable_x+", "+printable_y+")");
     }
 }
